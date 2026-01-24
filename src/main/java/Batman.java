3d860be83b6b665947d3f346b34aaf00986a2ebf
Bruce_Wayn <185772174+Hongyu1231@users.noreply.github.com>
2026-01-24 23:20:51 +0800

    1.Gave a new name to the chatbot 2.Implement an initial skeletal version

---
+++ src/main/java/Batman.java
@@ -0,0 +1,18 @@
+public class Batman {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String logo =
+                "      _==ZA==_      \n"
+                        + "    _/ ^  _  ^ \\_   \n"
+                        + "   | BAT  -  MAN |  \n"
+                        + "    \\_  _ _ _  _/   \n"
+                        + "      \\_  W  _/     \n";
+
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println(" Hello I'm Batman\n What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
