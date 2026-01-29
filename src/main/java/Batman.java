5edad2188202b3fa0a6d69c37b65b9e6b86a7733
Bruce_Wayn <185772174+Hongyu1231@users.noreply.github.com>
2026-01-29 14:09:15 +0800

    Week3 Level 2. Add, List

--- src/main/java/Batman.java
+++ src/main/java/Batman.java
@@ -18,6 +18,7 @@ public class Batman {
         System.out.println(logo);
         System.out.println("     Hello I'm Batman\n     What can I do for you?");
         printLine();
+        System.out.println();
     }
 
     public static void bye() {
@@ -27,15 +28,27 @@ public class Batman {
 
     public static void talk() {
         Scanner scanner = new Scanner(System.in);
+        String[]list = new String[100];
+        int taskNum;
+        int numOfTask = 0;
         while(true) {
             String input = scanner.nextLine();
             printLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                taskNum = 1;
+                for (int i = 0; i < numOfTask; i++) {
+                    System.out.println(" " + taskNum + ". " + list[i]);
+                    taskNum++;
+                }
             } else {
-                System.out.println("     " + input);
-                printLine();
+                System.out.println("     " + "added: " + input);
+                list[numOfTask] = input;
+                numOfTask++;
             }
+            printLine();
+            System.out.println();
         }
     }
 
