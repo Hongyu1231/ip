e5a6c6408e3bcc7504ee7a6ac4915f89b25b1f03
Bruce_Wayn <185772174+Hongyu1231@users.noreply.github.com>
2026-01-29 13:49:18 +0800

    Week3 Level 1. Echo

--- src/main/java/Batman.java
+++ src/main/java/Batman.java
@@ -1,18 +1,47 @@
-public class Batman {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String logo =
-                "      _==ZA==_      \n"
-                        + "    _/ ^  _  ^ \\_   \n"
-                        + "   | BAT  -  MAN |  \n"
-                        + "    \\_  _ _ _  _/   \n"
-                        + "      \\_  W  _/     \n";
+import java.util.Scanner;
 
+public class Batman {
+    public static void printLine() {
+        String line = "    ____________________________________________________________";
         System.out.println(line);
+    }
+
+    public static void greeting() {
+        String logo =
+                "          _==ZA==_      \n"
+                        + "        _/ ^  _  ^ \\_   \n"
+                        + "       | BAT  -  MAN |  \n"
+                        + "        \\_  _ _ _  _/   \n"
+                        + "          \\_  W  _/     \n";
+
+        printLine();
         System.out.println(logo);
-        System.out.println(" Hello I'm Batman\n What can I do for you?");
-        System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println("     Hello I'm Batman\n     What can I do for you?");
+        printLine();
+    }
+
+    public static void bye() {
+        System.out.println("     Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void talk() {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            printLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println("     " + input);
+                printLine();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        greeting();
+        talk();
+        bye();
     }
 }
