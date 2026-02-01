565869cfce125a8e4dcdcf85fe7215d69af9c891
unknown <185772174+Hongyu1231@users.noreply.github.com>
2026-02-01 16:47:47 +0800

    Week4 A-TextUiTesting: Automated Text UI Testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,21 +1,23 @@
 @ECHO OFF
 
-REM create bin directory if it doesn't exist
+REM Create a bin folder if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM delete output from previous run
+REM Delete previous output file
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
+REM Compile the code
+REM Note: We use *.java to compile Batman, Task, Todo, etc. all at once
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
 REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+REM Run the program, feed input.txt, and capture output to ACTUAL.TXT
+java -classpath ..\bin Batman < input.txt > ACTUAL.TXT
 
-REM compare the output to the expected output
+REM Compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
