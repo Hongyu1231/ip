b1b81441e719be5a39b8c5b8861350fae1a9d2ab
unknown <185772174+Hongyu1231@users.noreply.github.com>
2026-02-08 17:51:19 +0800

    Refactor: Split classes into batman, task, and exception packages

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,23 +1,23 @@
 @ECHO OFF
 
-REM Create a bin folder if it doesn't exist
+REM
 if not exist ..\bin mkdir ..\bin
 
-REM Delete previous output file
-if exist ACTUAL.TXT del ACTUAL.TXT
+REM
+del /Q ..\bin\batman\*.class
+del /Q ..\bin\task\*.class
+del /Q ..\bin\exception\*.class
 
-REM Compile the code
-REM Note: We use *.java to compile Batman, Task, Todo, etc. all at once
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+REM
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\batman\*.java ..\src\main\java\task\*.java ..\src\main\java\exception\*.java
 
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
-REM Run the program, feed input.txt, and capture output to ACTUAL.TXT
-java -classpath ..\bin Batman < input.txt > ACTUAL.TXT
+REM
+java -classpath ..\bin batman.Batman < input.txt > ACTUAL.TXT
 
-REM Compare the output to the expected output
+REM
 FC ACTUAL.TXT EXPECTED.TXT
